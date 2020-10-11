630fee5db12076267995f9e0610b48ee9d7f035b
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-10-11 19:05:53 +0900

    ブラウザ上にtodoを追加するロジックを構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { FormControl, TextField } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { db } from "./firebase";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 
 const App: React.FC = () => {
 	const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+	const [input, setInput] = useState("");
+
 	useEffect(() => {
 		const unSub = db.collection("tasks").onSnapshot((snapshot) => {
 			setTasks(
@@ -13,10 +17,32 @@ const App: React.FC = () => {
 		return () => unSub();
 	}, []);
 
+	const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+		db.collection("tasks").add({ title: input });
+		setInput("");
+	};
+
 	return (
 		<div className="App">
+			<h1>Todo App by React/Firebase</h1>
+			<FormControl>
+				<TextField
+					InputLabelProps={{
+						shrink: true,
+					}}
+					label="New task ?"
+					value={input}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+						setInput(e.target.value)
+					}
+				/>
+			</FormControl>
+			<button disabled={!input} onClick={newTask}>
+				<AddToPhotosIcon />
+			</button>
+
 			{tasks.map((task) => (
-				<h3>{task.title}</h3>
+				<h3 key={task.id}>{task.title}</h3>
 			))}
 		</div>
 	);
