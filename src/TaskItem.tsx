1484fd1fa84255cd52121713cc0ab6fe229d6dbe
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-10-11 19:34:02 +0900

    編集機能と削除機能を追加(ボタンconも)

--- src/TaskItem.tsx
+++ src/TaskItem.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import * as firebase from "firebase/app";
 import { ListItem, TextField, Grid } from "@material-ui/core";
 import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
 import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import { db } from "./firebase";
 
 interface PROPS {
 	id: string;
@@ -10,12 +11,38 @@ interface PROPS {
 }
 
 const TaskItem: React.FC<PROPS> = (props) => {
+	const [title, SetTitle] = useState(props.title);
+
+	const editTask = () => {
+		db.collection("tasks").doc(props.id).set({ title: title }, { merge: true });
+	};
+
+	const deleteTask = () => {
+		db.collection("tasks").doc(props.id).delete();
+	};
+
 	return (
-		<div>
-			<ListItem>
-				<h2>{props.title}</h2>
-			</ListItem>
-		</div>
+		<ListItem>
+			<h2>{props.title}</h2>
+			<Grid container justify="flex-end">
+				<TextField
+					InputLabelProps={{
+						shrink: true,
+					}}
+					label="Edit task"
+					value={title}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+						SetTitle(e.target.value)
+					}
+				/>
+			</Grid>
+			<button onClick={editTask}>
+				<EditOutlinedIcon />
+			</button>
+			<button onClick={deleteTask}>
+				<DeleteOutlineOutlinedIcon />
+			</button>
+		</ListItem>
 	);
 };
 
