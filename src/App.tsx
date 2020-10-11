1c1aaa68d2e4c95a95759d553b23528099b350a0
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-10-11 23:41:28 +0900

    cssの適用及びuseStylesを定義

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,23 @@ import styles from "./App.module.css";
 import { db } from "./firebase";
 import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 import TaskItem from "./TaskItem";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+	field: {
+		marginTop: 30,
+		marginBottom: 20,
+	},
+	list: {
+		margin: "auto",
+		width: "40%",
+	},
+});
 
 const App: React.FC = () => {
 	const [tasks, setTasks] = useState([{ id: "", title: "" }]);
 	const [input, setInput] = useState("");
+	const classes = useStyles();
 
 	useEffect(() => {
 		const unSub = db.collection("tasks").onSnapshot((snapshot) => {
@@ -24,10 +37,12 @@ const App: React.FC = () => {
 	};
 
 	return (
-		<div className="App">
+		<div className={styles.app__root}>
 			<h1>Todo App by React/Firebase</h1>
+			<br />
 			<FormControl>
 				<TextField
+					className={classes.field}
 					InputLabelProps={{
 						shrink: true,
 					}}
@@ -38,11 +53,11 @@ const App: React.FC = () => {
 					}
 				/>
 			</FormControl>
-			<button disabled={!input} onClick={newTask}>
+			<button className={styles.app__icon} disabled={!input} onClick={newTask}>
 				<AddToPhotosIcon />
 			</button>
 
-			<List>
+			<List className={classes.list}>
 				{tasks.map((task) => (
 					<TaskItem key={task.id} id={task.id} title={task.title} />
 				))}
